044e2cdbe35af7627ed2fffdceb02b2fdd2d9986
GiovanniBolivoro <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-02-04 09:35:46 -0300

    feat(images,about): add images and credit section
    Add image used on the site
    Add image credits section in About

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -3,11 +3,46 @@ import styles from "./AboutPage.module.css";
 export default function AboutPage() {
   return (
     <>
-      <h2>About</h2>
-      <p>
-        This is a simple blog build with <strong>React and Vite</strong> with placeholder content,
-        that means some classic lorem ipsum filler text.
-      </p>
+      <section className={styles.section}>
+        <h2>About</h2>
+        <p>
+          This is a simple blog build with <strong>React and Vite</strong> with placeholder content,
+          that means some classic lorem ipsum filler text.
+        </p>
+      </section>
+
+      <div className={styles.section}>
+        <h2>Image credits</h2>
+        <p>The images used are from Unsplash.</p>
+
+        <div className={styles.image_links}>
+          <a className="button" href="https://unsplash.com/photos/london-bridge-london-Q6UehpkBSnQ">
+            <img src={`${import.meta.env.BASE_URL}assets/images/london.png`} alt="" />
+            <span>Photo by Charles Postiaux</span>
+          </a>
+          <a
+            className="button"
+            href="https://unsplash.com/photos/statue-of-liberty-new-york-under-white-and-blue-cloudy-skies-PeFk7fzxTdk"
+          >
+            <img src={`${import.meta.env.BASE_URL}assets/images/new-york.png`} alt="" />
+            <span>Photo by Ferdinand Stöhr</span>
+          </a>
+          <a
+            className="button"
+            href="https://unsplash.com/photos/cityscape-photo-during-daytime-hIKVSVKH7No"
+          >
+            <img src={`${import.meta.env.BASE_URL}assets/images/new-zealand.png`} alt="" />
+            <span>Photo by Dan Freeman</span>
+          </a>
+          <a
+            className="button"
+            href="https://unsplash.com/photos/eiffel-tower-paris-france-nnzkZNYWHaU"
+          >
+            <img src={`${import.meta.env.BASE_URL}assets/images/paris.png`} alt="" />
+            <span>Photo by Chris Karidis</span>
+          </a>
+        </div>
+      </div>
     </>
   );
 }
